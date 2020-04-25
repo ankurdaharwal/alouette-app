ad93ad4cccea6314eb937b533ee59625467265fd
Jon <woodyjon@gmail.com>
2020-04-25 21:30:46 +0200

    WIP helicopter smart contract

--- smartcontract/HelicopterMoneyTest.sol
+++ smartcontract/HelicopterMoneyTest.sol
@@ -1,22 +1,128 @@
 pragma solidity ^0.6.3;
 
 contract HelicopterMoney{
-  struct wallet{
-    string typeOfuser; 
-    bool isInitiated;
-    string ID;
-    uint balance;
-    uint sentToTax;
+  struct Wallet{
+    bool isCitizen; // false: company 
+    bool isValid; // true: valid, false: invalid
+    bytes32 numIDCard;
+    bytes32 numCompany;
+    uint256 balance; // balance in € * 100
+    uint256 sentToTax;
   }
   
-  mapping(string => wallet) balancesUsers; //int256
-  mapping(string => wallet) balancesBusiness;
+  mapping(bytes32 => Wallet) citizensWallets;
+  mapping(bytes32 => Wallet) companiesWallets;
+  
+  bytes32[] listCitizensWallets;
+  bytes32[] listCompaniesWallets;
 
+  uint256 initialBalance = 100000; // 1000€
 
   constructor() public{
 
   }
-
+  
+  function creationOfCitizenWallet(bytes32 numIDCard, uint256 verificationKey) public {
+      
+    // check already exists
+    // check numIDCard and verificationKey
+    
+    listCitizensWallets.push(numIDCard);
+    
+    citizensWallets[numIDCard] = Wallet(
+      {
+        isCitizen: true,
+        isValid: true,
+        numIDCard: numIDCard,
+        numCompany: '',
+        balance: initialBalance,
+        sentToTax: 0
+      }
+    );
+  }
+  
+  function creationOfCompanyWallet(bytes32 numCompany, uint256 verificationKey) public {
+      
+    // check already exists
+    // check numCompany and verificationKey
+    
+    listCompaniesWallets.push(numCompany);
+    
+    citizensWallets[numCompany] = Wallet(
+      {
+        isCitizen: false,
+        isValid: true,
+        numIDCard: '',
+        numCompany: numCompany,
+        balance: 0,
+        sentToTax: 0
+      }
+    );
+  }
+  
+  
+  // below functions are limited to admin and authorities
+  
+  function getAllCitizensWallets() public view returns (bytes32[] memory) {
+      
+    return listCitizensWallets;
+  }
+  
+  function getCitizenWallet(bytes32 numIDCard) public view returns (bool, bool, bytes32, uint256) {
+      
+    return (citizensWallets[numIDCard].isCitizen, citizensWallets[numIDCard].isValid, citizensWallets[numIDCard].numIDCard, citizensWallets[numIDCard].balance);
+  }
+  
+  function getAllCompaniesWallets() public view returns (bytes32[] memory) {
+      
+    return listCompaniesWallets;
+  }
+  
+  function getCompanyWallet(bytes32 numCompany) public view returns (bool, bool, bytes32, uint256, uint256) {
+      
+    return (companiesWallets[numCompany].isCitizen, companiesWallets[numCompany].isValid, companiesWallets[numCompany].numCompany, companiesWallets[numCompany].balance, companiesWallets[numCompany].sentToTax);
+  }
+  
+  function validationOfCitizensWallets(bytes32[] memory listCitizensWalletsToValidate) public {
+    
+    for (uint i=0; i<listCitizensWalletsToValidate.length; i++) {
+      citizensWallets[listCitizensWalletsToValidate[i]].isValid = true;
+    }
+  }
+  
+  function invalidationOfCitizensWallets(bytes32[] memory listCitizensWalletsToInvalidate) public {
+    
+    for (uint i=0; i<listCitizensWalletsToInvalidate.length; i++) {
+      citizensWallets[listCitizensWalletsToInvalidate[i]].isValid = false;
+    }
+  }
+  
+  function validationOfCompanyWallets(bytes32[] memory listCompaniesWalletsToValidate) public {
+    
+    for (uint i=0; i<listCompaniesWalletsToValidate.length; i++) {
+      companiesWallets[listCompaniesWalletsToValidate[i]].isValid = true;
+    }
+  }
+  
+  function invalidationOfCompanyWallets(bytes32[] memory listCompaniesWalletsToInvalidate) public {
+    
+    for (uint i=0; i<listCompaniesWalletsToInvalidate.length; i++) {
+      companiesWallets[listCompaniesWalletsToInvalidate[i]].isValid = false;
+    }
+  }
+  
+  
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+/*
   function creationOfIndividualWallet(string memory ID, uint balance) public {
     balancesUsers[ID] = wallet("citizen", true, ID, balance,0);
   }
@@ -43,5 +149,5 @@ contract HelicopterMoney{
   function returntestValue (uint testValue) public view returns(uint) {
     		return testValue;
   }
+  */
 
-}
